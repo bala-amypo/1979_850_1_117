ef2d26d8dcb1d2b0ed943c7756e1c4af7b3a43c8
bala-amypo <bala@amypo.in>
2025-12-26 06:24:32 +0000

    User commit

--- src/main/java/com/example/demo/service/DeviceProfileService.java
+++ src/main/java/com/example/demo/service/DeviceProfileService.java
@@ -1,17 +1,44 @@
-package com.example.demo.service;
+package com.example.demo.service.impl;
 
 import com.example.demo.entity.DeviceProfile;
+import com.example.demo.repository.DeviceProfileRepository;
+import com.example.demo.service.DeviceProfileService;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface DeviceProfileService {
+@Service
+public class DeviceProfileServiceImpl implements DeviceProfileService {
 
-    DeviceProfile registerDevice(DeviceProfile device);
+    private final DeviceProfileRepository deviceRepo;
 
-    DeviceProfile updateTrustStatus(Long id, boolean trust);
+    public DeviceProfileServiceImpl(DeviceProfileRepository deviceRepo) {
+        this.deviceRepo = deviceRepo;
+    }
 
-    List<DeviceProfile> getDevicesByUser(Long userId);
+    @Override
+    public DeviceProfile registerDevice(DeviceProfile device) {
+        return deviceRepo.save(device);
+    }
 
-    Optional<DeviceProfile> findByDeviceId(String deviceId);
+    @Override
+    public DeviceProfile updateTrustStatus(Long id, boolean trust) {
+        DeviceProfile device = deviceRepo.findById(id).orElse(null);
+        if (device != null) {
+            device.setIsTrusted(trust);
+            return deviceRepo.save(device);
+        }
+        return null;
+    }
+
+    @Override
+    public List<DeviceProfile> getDevicesByUser(Long userId) {
+        return deviceRepo.findByUserId(userId);
+    }
+
+    @Override
+    public Optional<DeviceProfile> findByDeviceId(String deviceId) {
+        return deviceRepo.findByDeviceId(deviceId);
+    }
 }
