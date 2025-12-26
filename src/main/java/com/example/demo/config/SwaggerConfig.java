1ae30f978c4bf9b6c189543b39d7a37b3d243edd
bala-amypo <bala@amypo.in>
2025-12-26 13:09:53 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,14 +1,9 @@
 package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
-import io.swagger.v3.oas.models.Components;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import java.util.List;
 
 @Configuration
@@ -17,24 +12,9 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                // API basic info
-                .info(new Info()
-                        .title("IT Policy Violation Detection API")
-                        .version("1.0.0")
-                        .description("API for IT Policy Violation Detection System with JWT Authentication"))
-
-                // Server URL
+                // You need to change the port as per your server
                 .servers(List.of(
                         new Server().url("https://9280.408procr.amypo.ai/")
-                ))
-
-                // JWT Security
-                .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
-                .components(new Components()
-                        .addSecuritySchemes("Bearer Authentication",
-                                new SecurityScheme()
-                                        .type(SecurityScheme.Type.HTTP)
-                                        .scheme("bearer")
-                                        .bearerFormat("JWT")));
-    }
+                ));
+        }
 }
