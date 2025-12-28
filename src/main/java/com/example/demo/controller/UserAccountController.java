05aac76e7d26400d86297ffbed1e072ed9873154
bala-amypo <bala@amypo.in>
2025-12-28 05:39:19 +0000

    User commit

--- src/main/java/com/example/demo/controller/UserAccountController.java
+++ src/main/java/com/example/demo/controller/UserAccountController.java
@@ -3,25 +3,20 @@ package com.example.demo.controller;
 import com.example.demo.entity.UserAccount;
 import com.example.demo.service.UserAccountService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 
-@RestController
-@RequestMapping("/users")
 public class UserAccountController {
 
-    private final UserAccountService service;
+    private final UserAccountService userService;
 
-    public UserAccountController(UserAccountService service) {
-        this.service = service;
+    public UserAccountController(UserAccountService userService) {
+        this.userService = userService;
     }
 
-    @PostMapping
-    public ResponseEntity<UserAccount> create(@RequestBody UserAccount user) {
-        return ResponseEntity.ok(service.createUser(user));
+    public ResponseEntity<UserAccount> create(UserAccount user) {
+        return ResponseEntity.ok(userService.createUser(user));
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<UserAccount> get(@PathVariable Long id) {
-        return ResponseEntity.ok(service.getUserById(id));
+    public ResponseEntity<UserAccount> getById(Long id) {
+        return ResponseEntity.ok(userService.getUserById(id));
     }
 }
